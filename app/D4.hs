8ba1d9e5a54e8adf79da647ebe2005ee971e17e5
Nikola Kasev <nikola.kasev@gmail.com>
2019-12-05 20:56:05 +0100

    P2D4, there must be a better way to explain a single group with two members

--- app/D4.hs
+++ app/D4.hs
@@ -20,6 +20,12 @@ solution =
   , c <= d
   , d <= e
   , e <= f
+  , (a == b) && (b /= c) && (b /= d) && (b /= e) && (b /= f) ||
+      (b == c) && (b /= a) && (b /= d) && (b /= e) && (b /= f) ||
+      (c == d) && (a /= c) && (b /= c) && (c /= e) && (c /= f) ||
+      (d == e) && (a /= d) && (b /= d) && (d /= c) && (d /= f) ||
+      (e == f) && (a /= e) && (b /= e) && (c /= e) && (d /= f)
   , Set.size (Set.fromList [a, b, c, d, e, f]) <= 5
-  , let total = a*10^5 + b*10^4 + c*10^3 + d*10^2 + e*10 + f in total >= 266666 && total <= 799999
+  , let total = a * 10 ^ 5 + b * 10 ^ 4 + c * 10 ^ 3 + d * 10 ^ 2 + e * 10 + f
+     in total >= 266666 && total <= 799999
   ]
